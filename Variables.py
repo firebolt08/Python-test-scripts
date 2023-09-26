930c80fff976aaffe071b9ad3ec23a38100de700
firebolt08 <zorzac2001@gmail.com>
2023-09-26 13:29:39 -0500

    Added some new python scripts that I did in my module.

---
+++ Variables.py
@@ -0,0 +1,12 @@
+var = "3.8.5"
+print("Python version: "+var)
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5
+print("c =", c)
+john = 3
+mary = 5
+adam = 6
+print (john, mary, adam)
+total_apples = john+mary+adam
+print (total_apples)
