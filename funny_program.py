495041fb33d11f638fcff128911d1cf66dd4aaae
firebolt08 <zorzac2001@gmail.com>
2023-09-29 11:06:23 -0500

    Added files for module 3.

---
+++ funny_program.py
@@ -0,0 +1,9 @@
+# input statement:
+inputString = input( "I'm waiting: ")
+# if condition here:
+if inputString == "Spathiphyllum":
+    print("Yes - Spathiphyllum is the best plant ever!")
+elif inputString == "spathiphyllum":
+    print("No, I want a big Spathiphyllum!")
+else:
+    print("Spathiphyllum! Not" + inputString + "!")
